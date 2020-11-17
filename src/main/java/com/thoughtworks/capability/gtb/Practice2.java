093206625dcb5d16bf48bbf7a3c4a85768916e34
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-17 20:05:28 +0800

    practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.SUNDAY;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -8,9 +12,21 @@ import java.time.LocalDate;
  * @author itutry
  * @create 2020-05-15_17:20
  */
+
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    LocalDate localDate = date.plusDays(1);
+    DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+    if (dayOfWeek==DayOfWeek.SATURDAY){
+      return localDate.plusDays(2);
+    }
+    if (dayOfWeek==DayOfWeek.SUNDAY){
+      return localDate.plusDays(1);
+    }
+    else {
+      return localDate;
+    }
   }
 }
+
